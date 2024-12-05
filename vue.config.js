ccb45c750960e9b541d55de53a79b28eb4a48e41
publishnsodqmodd <14721992+publishnsodqmodd@user.noreply.gitee.com>
2024-12-05 08:11:47 +0800

    添加第三方库移除代理服务器

--- vue.config.js
+++ vue.config.js
@@ -4,17 +4,3 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, //关闭 eslint校验
 });
-// 代理服务器配置
-// module.exports = {
-//   devServer: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:8080",
-//         changeOrigin: true,
-//         pathRewrite: {
-//           "^/api": "",
-//         },
-//       },
-//     },
-//   },
-// };

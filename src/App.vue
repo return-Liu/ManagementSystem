906b99c872f48662bea453799f3767bfc7267376
publishnsodqmodd <14721992+publishnsodqmodd@user.noreply.gitee.com>
2024-12-05 08:10:59 +0800

    修改图标添加加载线

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <div>
-    <!-- 给路由出口添加过渡效果 -->
-    <transition>
-      <router-view></router-view>
-    </transition>
+  <div class="container">
+    <div v-show="loading" class="loading-line"></div>
+    <router-view></router-view>
   </div>
 </template>
+
 <script>
 export default {
   name: "App",
   data() {
     return {
-      // 获取本地存储的主题
-      theme: localStorage.getItem("theme") || "light",
+      theme: localStorage.getItem("theme"),
+      // 表示加载状态
+      loading: false,
+      // 表示存储加载定时器
+      loadingTimer: null,
+      // 默认加载时间
+      loadDuration: 500,
     };
   },
   created() {
-    //当页面创建时 设置主题
+    console.log("Router:", this.$router);
+    if (!this.$router) {
+      console.error("Vue Router is not defined");
+      return;
+    }
     document.documentElement.setAttribute("data-theme", this.theme);
+    // to 表示在那个路由 from 表示去那个路由 next跳过以上继续执行
+    this.$router.beforeEach((to, from, next) => {
+      // 当路由进行加载时 将加载状态啊为true
+      this.loading = true;
+      // 如果存在之前的加载定时器
+      if (this.loadingTimer) {
+        // 清除
+        clearTimeout(this.loadingTimer);
+      }
+      // 以上都跳过 继续执行
+      next();
+    });
+    this.$router.afterEach((to, from) => {
+      // 记录路由切换信息。
+      console.log("路由切换", to, from);
+      this.loadDuration = 500; // 重置默认加载时间
+      // 启动加载动画
+      this.startLoadingAnimation();
+    });
+  },
+  methods: {
+    startLoadingAnimation() {
+      // 记录当前时间
+      const startTime = Date.now();
+      // 确保DOM更新后再执行加载动画
+      this.$nextTick(() => {
+        // 设置一个定时器，在指定时间后执行
+        this.loadingTimer = setTimeout(() => {
+          // 记录结束时间
+          const endTime = Date.now();
+          // 计算实际的加载时间
+          const actualDuration = endTime - startTime;
+          // 确保加载时间不少于 500ms
+          this.loadDuration = Math.max(500, actualDuration);
+          // 关闭加载状态
+          this.loading = false;
+        }, this.loadDuration);
+      });
+    },
   },
 };
 </script>
+
 <style lang="less">
 html,
 body,
@@ -33,12 +81,31 @@ p {
 body {
   background-color: var(--bg1);
   color: var(--text-color);
+  margin: 0 auto;
 }
-// 处理卡片内的样式
-/* 使用:deep()穿透Element UI组件的封装 */
 :deep(.el-card__body),
 :deep(.el-main) {
   padding: 20px;
-  margin-bottom: 0; /* 避免负margin影响外部布局 */
+  margin-bottom: 0;
+}
+
+.loading-line {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 2px;
+  background-color: #43b3cf;
+  z-index: 1000;
+  width: 0;
+  animation: load 1s forwards;
+}
+
+@keyframes load {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -9,7 +9,9 @@ import store from "./store";
 // 引入mock
 import "./api/mock";
 // 引入主题样式
-import "./style/theme.css";
+import "./sass_style/theme.scss";
+// 引入sass星空
+import "./sass_style/StarrySky.scss";
 // 引入cookie
 import cookie from "js-cookie";
 // 全局引入 element ui库
@@ -17,23 +19,29 @@ import ElementUI from "element-ui";
 // 引入Element UI的默认主题样式
 import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
-//   添加全局前置守卫
+// 引入阿里巴巴矢量图标库
+import "./assets/icon/iconfont.css";
+// 引入数据可视化
+import dataV from "@jiaminghi/data-view";
+// 使用插件
+Vue.use(dataV);
+// 引入动画样式
+import "animate.css";
+// 添加全局前置守卫
 router.beforeEach((to, from, next) => {
-  // 判断是否有token
   const token = cookie.get("token");
-  if (!token && to.name !== "login" && to.name !== "register") {
+  if (!token && to.path !== "/login/index" && to.path !== "/register") {
     // 如果token不存在 则跳转登录页面
-    next({ name: "login" });
+    next({ path: "/login/index" });
     // 判断token存在 则跳转首页
-  } else if (token && to.name == "login") {
-    next({ name: "home" });
-  } else if (token && to.name == "register") {
-    next({ name: "login" });
+  } else if (token && to.path == "/login/index") {
+    next({ path: "/home" });
+  } else if (token && to.path == "/register") {
+    next({ path: "login/index" });
   } else {
     next();
   }
 });
-
 // 创建vue实例
 new Vue({
   router,
@@ -41,6 +49,7 @@ new Vue({
   render: (h) => h(App),
   // 在vue实例上创建动态添加菜单
   created() {
+    // 动态添加菜单
     store.commit("addMenu", router);
   },
 }).$mount("#app");
